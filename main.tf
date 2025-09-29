b42c7f1d1013c30e79dd57599af79936b00f7010
anupjacob <anup.jacob@outlook.com>
2025-09-29 12:32:44 +0100

    Adding new resources for test

--- main.tf
+++ main.tf
@@ -68,3 +68,30 @@ resource "azurerm_linux_virtual_machine" "anup_vm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.rg_name.id}-aks"
+  location            = azurerm_resource_group.anup-test-rg.location
+  resource_group_name = azurerm_resource_group.anup-test-rg.name
+  dns_prefix          = "${random_pet.rg_name.id}-k8s"
+  kubernetes_version  = "1.26.3"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "Demo Environment"
+  }
+}
+
