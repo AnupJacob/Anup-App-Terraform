c0709225e9128aa853d95f9b0af2b5bb79184a37
anupjacob <anup.jacob@outlook.com>
2025-09-30 17:57:36 +0100

    Updating the vm size for vcpu quota fix

--- main.tf
+++ main.tf
@@ -78,9 +78,8 @@ resource "azurerm_kubernetes_cluster" "default" {
 
   default_node_pool {
     name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_A2_v2"
-    os_disk_size_gb = 30
+    node_count      = 1
+    vm_size         = "Standard_B2s"
   }
 
   identity {
